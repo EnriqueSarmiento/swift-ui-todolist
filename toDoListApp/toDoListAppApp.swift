48fc8f75569957838e97b41985828c895ff8b85b
Enrique Sarmiento <jeisonsarmiento@correo.unimet.edu.ve>
2023-09-18 22:01:25 -0400

    Initial Commit

---
+++ toDoListApp/toDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListAppApp.swift
+//  toDoListApp
+//
+//  Created by Enrique Sarmiento on 18/9/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
